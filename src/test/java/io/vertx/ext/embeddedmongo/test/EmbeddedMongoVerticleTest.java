33e4576e7cec6e5b5f403459677d7ba4ef053b9e
Josef Pfleger <green-carrot@orange-cactus.org>
2016-12-28 18:17:06 +0100

    add some tests for dynamic port

--- src/test/java/io/vertx/ext/embeddedmongo/test/EmbeddedMongoVerticleTest.java
+++ src/test/java/io/vertx/ext/embeddedmongo/test/EmbeddedMongoVerticleTest.java
@@ -16,7 +16,10 @@
 
 package io.vertx.ext.embeddedmongo.test;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.embeddedmongo.EmbeddedMongoVerticle;
 import io.vertx.test.core.VertxTestBase;
 import org.junit.Test;
 
@@ -36,11 +39,56 @@ public class EmbeddedMongoVerticleTest extends VertxTestBase {
     // Not really sure what to test here apart from start and stop
     vertx.deployVerticle("service:io.vertx.vertx-mongo-embedded-db", onSuccess(deploymentID -> {
       assertNotNull(deploymentID);
-      vertx.undeploy(deploymentID, onSuccess(v -> {
-        assertNull(v);
-        testComplete();
-      }));
+      undeploy(deploymentID);
     }));
     await();
+  }
+
+  @Test
+  public void testConfiguredPort() {
+    EmbeddedMongoVerticle mongo = new EmbeddedMongoVerticle();
+    vertx.deployVerticle(mongo, createOptions(7533), onSuccess(deploymentID -> {
+      assertNotNull(deploymentID);
+      assertEquals(7533, mongo.actualPort());
+      undeploy(deploymentID);
+    }));
+    await();
+  }
+
+  @Test
+  public void testRandomPort() {
+    EmbeddedMongoVerticle mongo = new EmbeddedMongoVerticle();
+    vertx.deployVerticle(mongo, createOptions(0), onSuccess(deploymentID -> {
+      assertNotNull(deploymentID);
+      assertNotSame(0, mongo.actualPort());
+      undeploy(deploymentID);
+    }));
+    await();
+  }
+
+  @Test
+  public void testRandomPortNoConfig() {
+    EmbeddedMongoVerticle mongo = new EmbeddedMongoVerticle();
+    vertx.deployVerticle(mongo, createEmptyOptions(), onSuccess(deploymentID -> {
+      assertNotNull(deploymentID);
+      assertNotSame(0, mongo.actualPort());
+      undeploy(deploymentID);
+    }));
+    await();
+  }
+
+  private DeploymentOptions createOptions(int port) {
+    return createEmptyOptions().setConfig(new JsonObject().put("port", port));
+  }
+
+  private DeploymentOptions createEmptyOptions() {
+    return new DeploymentOptions().setWorker(true);
+  }
+
+  private void undeploy(String deploymentID) {
+    vertx.undeploy(deploymentID, onSuccess(v -> {
+      assertNull(v);
+      testComplete();
+    }));
   }
 }
